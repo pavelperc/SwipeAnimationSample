b5bf6896559e89517bed45e9a41b94edb7cc2634
pavelperc <pavelperc@mail.ru>
2023-05-07 17:33:10 +0300

    init. working bad.

---
+++ app/src/main/java/com/example/motionlayoutsample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.motionlayoutsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.motionlayoutsample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+    }
+}
